243cad0a734c11264c4e77d7952e90aca0f9ab4b
Juliette Reeder <111520182+jureeder@users.noreply.github.com>
2025-04-24 14:14:21 -0500

    Update detect_fm_signal.py

--- detect_fm_signal.py
+++ detect_fm_signal.py
@@ -10,10 +10,10 @@ sdr = RtlSdr()
 sdr.sample_rate = 2.4e6      # Sampling rate in Hz
 sdr.center_freq = 100.1e6    # Center frequency in Hz, change for each FM station 
 
-sdr.center_freq = 92.1e6     # The Beat
-sdr.center_freq = 93.7e6     # Hot 93.7
-sdr.center_freq = 96.5e6     # Mixed 96.5
-sdr.center_freq = 98.9e6     # KissFM
+#sdr.center_freq = 92.1e6     # The Beat
+#sdr.center_freq = 93.7e6     # Hot 93.7
+#sdr.center_freq = 96.5e6     # Mixed 96.5
+#sdr.center_freq = 98.9e6     # KissFM
 
 sdr.gain = 'auto'            # Use automatic gain control 
 
@@ -27,3 +27,9 @@ sdr.close()
 # === Save Raw IQ Data ===
 np.save('raw_signal.npy', samples)
 print("Signal acquisition complete. Saved as 'raw_signal.npy'")
+
+
+
+
+
+
